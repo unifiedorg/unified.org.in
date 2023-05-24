ccbad6dbfc55d33322e0722affca48085e287766
Unium <81354905+TheUnium@users.noreply.github.com>
2023-05-24 23:00:48 +0530

    add(docsheader.php): Added documentation header
    Signed-off-by: Unium <81354905+TheUnium@users.noreply.github.com>

---
+++ app/components/docsheader.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="shortcut icon" href="<?= $favicon ?>" type="image/x-icon">
+    <link rel="stylesheet" href="/api/getAsset/css/doc">
+    <link rel="stylesheet" href="/api/getAsset/css/docglobal">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900" rel="stylesheet">
+  </head>
+  <body >

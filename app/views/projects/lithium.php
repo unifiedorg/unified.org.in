3f97e7943449d0558b1444bb85408fd70a6bdb64
Unium <81354905+TheUnium@users.noreply.github.com>
2023-05-24 23:09:14 +0530

    update(lithium.php): Added link to documentation
    Signed-off-by: Unium <81354905+TheUnium@users.noreply.github.com>

--- app/views/projects/lithium.php
+++ app/views/projects/lithium.php
@@ -16,13 +16,13 @@
           class="rounded-lg max-w-xl hidden md:block"
           >
         <div class="mt-8 flex flex-wrap justify-center gap-4">
-          <a href="#readmore" class="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+          <a href="/project/lithium/docs" class="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
           <span class="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
           <span class="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
           <span class="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
           <span class="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
           </span>
-          <span class="relative text-white">Read more</span>
+          <span class="relative text-white">Documentation</span>
           </a>
           <a href="#download" class="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
           <span class="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-purple-500 rounded-full blur-md ease"></span>

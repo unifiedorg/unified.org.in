a7f896800e2c98b1a7e2cf303d78fc596bf72869
unium <cyberbot90000@gmail.com>
2023-05-24 13:39:13 +0530

    update(./app/config): Updated config files to have the appropriate information

--- app/config/app.php
+++ app/config/app.php
@@ -1,6 +1,6 @@
 <?php
-if (!defined('APP_NAME')){                       define('APP_NAME', 'example-app');}
+if (!defined('APP_NAME')){                       define('APP_NAME', 'unified-web');}
 if (!defined('APP_VERSION')){                    define('APP_VERSION', 'v1.0.0');}
 if (!defined('LITHIUM_VERSION')){                define('LITHIUM_VERSION', 'v1.0.0');}
 if (!defined('APP_AUTHOR')){                     define('APP_AUTHOR', 'Unified');}
-if (!defined('APP_DESCRIPTION')){                define('APP_DESCRIPTION', 'A basic app using Lithium.');}
+if (!defined('APP_DESCRIPTION')){                define('APP_DESCRIPTION', 'Main website of Unified (https://unified.org.in).');}
